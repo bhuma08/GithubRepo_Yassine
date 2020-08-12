6b352d3fc431d5b0787ada93838e4b68bf68333a
bhuma08 <bhumalimbu08@gmail.com>
2020-08-12 12:31:06 +0100

    update

--- src/Components/Githubapi.js
+++ src/Components/Githubapi.js
@@ -4,34 +4,43 @@ import React, { Component } from 'react';
 class Githubapi extends Component {
     state = {
         practice : " ",
-        // load : false
+        load : true
     }
 
     //api = ()=> {
-    componentDidMount(){
-        console.log("I've been called");
-        const url = `https://api.github.com/users/${this.props.userName}/repos`;
-        //const response = fetch(url);
-        fetch(url)
-        .then(resp => resp.json())
-        .then(data => console.log(data))
-        .then(data => this.setState({ practice : data, loading : false }))
-        //const data = response.json();
-        //console.log(data);
-        //this.setState({practice: data, load : false})
+    // componentDidMount(){
+    //     console.log("I've been called");
+    //     const url = `https://api.github.com/users/${this.props.userName}/repos`;
+    //     //const response = fetch(url);
+    //     fetch(url)
+    //     .then(resp => resp.json())
+    //     .then(data => console.log(data))
+    //     .then(data => this.setState({ practice : data, loading : false }))
+    //     //const data = response.json();
+    //     //console.log(data);
+    //     //this.setState({practice: data, load : false})
 
+    // }
+
+    async componentDidMount(){
+        const url = `https://api.github.com/users/${this.props.userName}/repos`;
+        // const url = `https://api.github.com/users/bhuma08/repos`;
+        const response = await fetch(url);
+        const data = await response.json();
+        this.setState({practice: data, load: false})
+        console.log(data[1]);
     }
 
     render(){
         return(
             <div>
-                {/* { this.api() } */}
-                {/* { this.state.load || !this.state.practice ? (
+                { this.state.load || !this.state.practice ? (
                 <div>loading...</div>
-                ) : ( */}
+                ) : (
                 <div>
                     No of Repo: {this.state.practice.length}<br></br>
-                    Name of Repo: {this.state.practice[0].name}
+                    Name of Repo 1: {this.state.practice[0].name}<br></br>
+                    Name of Repo 2: {this.state.practice[0].id} 
                 </div>
                 )} 
             </div>
--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,35 +1,40 @@
 import React, { Component } from 'react';
 import Githubapi from './Githubapi';
-
 class Search extends Component {
-    state = { username: "" }    
-    
-    // handleChange = e => this.setState({ username: e.target.value }) 
-
+    state = { 
+        username: "" ,
+        loading: true,
+        error: true
+    }    
+    handleChange = e => this.setState({ username: e.target.value }) 
     // Handlesubmit should save userinput in a variable to be passed 
     handleSubmit = e => {
         e.preventDefault();
-        this.setState({ username: e.target.value });
+        this.setState(prevState => ({
+            username: prevState.username
+        }))
+        this.setState({error: false})
+        this.setState({loading: false})
+        // this.setState({ username: e.target.userInput.value });
+        console.log(this.state.username)
     }
-    
+    renderResult = () => {this.loading ? <p>loading...</p> : <Githubapi userName={this.state.username}/>}
     render(){
-
-        
-        
         return(
             <div>
-                <form>
-                    <input type="text" name="userInput" placeholder="Enter Github Username"></input>
-                    <input type="submit" name="submitUserInput" onSubmit={this.handleSubmit}></input>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="userInput" id="userInput" onChange={this.handleChange} placeholder="Enter Github Username" ></input>
+                    <input type="submit" name="submitUserInput" ></input>
                 </form> 
-                {console.log(this.state.username)}
-                <Githubapi userName={this.state.username}/> 
+                <br></br>
+                <div>
+                Username: {this.state.username}
+                { this.state.error ? <p></p> : this.renderResult() }
+                </div>
             </div>  
         )
     }
-    
 }
-
 export default Search;
     
 
